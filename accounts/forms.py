413c5974721fa83a120b7c5791f4d946c05ffa93
SkaeX <abdulmajid.hamza@aun.edu.ng>
2017-04-20 16:38:00 +0100

    Updated user signup form
    - Add first name and last name to signup form.
    - Smashed a bug in email confirmation template.

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,7 +8,12 @@ class MayoSignupForm(forms.Form):
         ('Mentor', 'I am here to mentor'),
     )
     role = forms.ChoiceField(choices=roles, label='Role', initial=roles[0][0])
+    first_name = forms.CharField(max_length=50, label='First Name', widget=forms.TextInput(attrs={'autofocus': 'autofocus'}))
+    last_name = forms.CharField(max_length=50, label='Last Name')
 
     def signup(self, request, user):
         grp = Group.objects.get(name=self.cleaned_data['role'])
         user.groups.add(grp)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.save()
