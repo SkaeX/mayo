9c99468cb1c6214726a474fd0945e844dd0d9072
SkaeX <abdulmajid.hamza@aun.edu.ng>
2017-05-05 14:47:48 +0100

    Still disabling

--- mayo/settings/production.py
+++ mayo/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 # DEBUG
 # ------------------------------------------------------------------------------
-DEBUG = env.bool('DJANGO_DEBUG', default=True)
+DEBUG = env('DJANGO_DEBUG', default=True)
 TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 
 # SECRET CONFIGURATION
